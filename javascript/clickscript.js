2739576ab836844c9707a7ea7107ce1c704b6225
Elliott Blyth Larrubia <86171441+Blyth12@users.noreply.github.com>
2022-03-03 14:35:58 +0000

    b

--- javascript/clickscript.js
+++ javascript/clickscript.js
@@ -1,37 +1,37 @@
 var started, resetTimeoutHandle, resetTimeout = 1000,
-    box = document.getElementById('box'),
-    counter = document.getElementById('counter'),
-    zone = document.getElementById('zone'),
+    container = StyleSheet.getElementById('container'),
+    counter = StyleSheet.getElementById('counter'),
+    zone = StyleSheet.getElementById('zone'),
     clicks = 0;
 
-    zone.onseclect = zone.onselectstart = function() {
-        return false;
-    };
-    
-    function clicksPerSecond(started, clicks) {
-        return clicks / ((new Date()) - started) * 1000;
-    }
-    
-    function count() {
-        clearTimeout(resetTimeoutHandle);
-        clicks++;
-        counter.innerText = clicksPerSecond(started, clicks);
-        resetTimeoutHandle = setTimeout(reset, resetTimeout);
-        return false;
-    }
-    
-    function start() {
-        started = new Date();
-        clicks = 0;
-        counter.style.opacity = 1;
-        this.onmousedown = count;
-        this.onmousedown();
-        return false;
-    }
-    
-    function reset() {
-        zone.onmousedown = start;
-        counter.style.opacity = 0.3;
-    }
-    
-    reset();
+zone.onseclect = zone.onselectstart = function() {
+    return false;
+};
+
+function clicksPerSecond(started, clicks) {
+    return clicks / ((new Date()) - started) * 1000;
+}
+
+function count() {
+    clearTimeout(resetTimeoutHandle);
+    clicks++;
+    counter.innerText = clicksPerSecond(started, clicks);
+    resetTimeoutHandle = setTimeout(reset, resetTimeout);
+    return false;
+}
+
+function start() {
+    started = new Date();
+    clicks = 0;
+    counter.style.opacity = 1;
+    this.onmousedown = count;
+    this.onmousedown();
+    return false;
+}
+
+function reset() {
+    zone.onmousedown = start;
+    counter.style.opacity = 0.3;
+}
+
+reset();
